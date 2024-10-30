e113e7c157ea9e1b31df0a0373b43ec99325f37e
navincograph179 <navin_gurung@co-graph.com>
2024-10-30 21:21:13 +0900

    collection.jsx tasks

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -8,6 +8,23 @@ const Collection = () => {
   const { products } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(true)
   const [ filterProducts, setFilterProducts ] = useState([]);
+  // logic for filtered
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+
+  const toggleCategory = (e) => {
+    if(category.includes(e.target.value)) {
+      setCategory(prev => prev.filter(item => item !== e.target.value))
+    }
+    else {
+      setCategory(prev =>[...prev, e.target.value])
+    }
+  }
+// [category] means,for save the updated on category array.
+  useEffect(()=> {
+    console.log(category)
+  },[category])
+
 
   useEffect(()=> {
     setFilterProducts(products)
@@ -25,13 +42,13 @@ const Collection = () => {
           <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Men'} /> Men
+              <input className='w-3' type="checkbox" value={'Men'} onChange={toggleCategory} /> Men
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Women'} /> Women
+              <input className='w-3' type="checkbox" value={'Women'} onChange={toggleCategory}/> Women
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Kids'} /> Kids
+              <input className='w-3' type="checkbox" value={'Kids'} onChange={toggleCategory}/> Kids
             </p>
           </div>
         </div>
