668f107ebe5d8b3cf567fb2f668e8ad587f9085d
navincograph179 <navin_gurung@co-graph.com>
2024-11-06 22:51:58 +0900

    category filter

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -20,15 +20,32 @@ const Collection = () => {
       setCategory(prev =>[...prev, e.target.value])
     }
   }
+
+  const toggleSubCategory = (e) => {
+    if(subCategory.includes(e.target.value)) {
+      setSubCategory(prev => prev.filter(item => item !== e.target.value))
+    }
+    else {
+      setSubCategory(prev => [...prev, e.target.value])
+    }
+  }
+
+  const applyFilter =() => {
+    let productsCopy = products.slice(); //slice for copy the orginal data
+    if(category.length > 0) {
+      productsCopy = productsCopy.filter(item => category.includes(item.category))
+    }
+    if(subCategory.length > 0) {
+      productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory))
+    }
+    setFilterProducts(productsCopy)
+  }
+
 // [category] means,for save the updated on category array.
-  useEffect(()=> {
-    console.log(category)
-  },[category])
-
-
-  useEffect(()=> {
-    setFilterProducts(products)
-  }, [])
+  
+  useEffect(()=>{
+    applyFilter()
+  }, [category, subCategory])
   
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -57,13 +74,13 @@ const Collection = () => {
           <p className='mb-3 text-sm font-medium'>TYPE</p>
           <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Topwear'} /> Topwear
+              <input className='w-3' type="checkbox" value={'Topwear'} onChange={toggleSubCategory}/> Topwear
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Bottomwear'} /> Bottomwear
+              <input className='w-3' type="checkbox" value={'Bottomwear'} onChange={toggleSubCategory}/> Bottomwear
             </p>
             <p className='flex gap-2'>
-              <input className='w-3' type="checkbox" value={'Winterwear'} /> Winterwear
+              <input className='w-3' type="checkbox" value={'Winterwear'} onChange={toggleSubCategory}/> Winterwear
             </p>
           </div>
         </div>
