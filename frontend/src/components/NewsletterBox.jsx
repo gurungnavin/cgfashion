a89745cfcc565fd7db57e3e7ca49a56a0f38d983
navincograph179 <navin_gurung@co-graph.com>
2024-12-23 17:13:12 +0900

    into japanese

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -6,11 +6,10 @@ const NewsletterBox = () => {
   return (
     <div className="text-center">
       <p className="text-2xl font-medium text-gray-800">
-        Subscribe now & get 20% off
+      今すぐ購読して20%オフ！
       </p>
       <p className="text-gray-400 mt-3">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry.
+      限定オファー！今すぐ購読して、次回のお買い物で20%オフをゲット。最新商品情報や特別割引をお届けします。お見逃しなく！
       </p>
       <form className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3">
         <input className="w-full sm:flex-1 outline-none" type="email" placeholder="Enter your Email" required/>
