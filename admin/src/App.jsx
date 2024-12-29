9d48c53d5fc2e5fb2b94687b4f335e0668f18aa3
navincograph179 <navin_gurung@co-graph.com>
2024-12-29 21:08:48 +0900

    testing

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SideBar from './components/Siderbar';
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add';
+import List from './pages/List';
+import Orders from './pages/Orders';
 
 const App = () => {
   return (
@@ -12,6 +16,13 @@ const App = () => {
     <hr />
     <div className='flex w-full'>
       <SideBar />
+      <div className='w-70% mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base'>
+        <Routes>
+          <Route path='/add' element = {<Add />} />
+          <Route path='/list' element = {<List />} />
+          <Route path='/orders' element = {<Orders />} />
+        </Routes>
+      </div>
     </div>
     </>
     </div>
--- admin/src/components/Siderbar.jsx
+++ admin/src/components/Siderbar.jsx
@@ -4,19 +4,19 @@ import { assets } from '../assets/assets'
 
 const Siderbar = () => {
   return (
-    <div className="w-[18%] min-h-screen border-r-2">
+    <div className="w-[19%] min-h-screen border-r-2">
       <div className="flex flex-col gap-4 pt-6 pl-[20%] text-[15px]">
-        <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-4 py-2 rounded-l-lg" to='/add'>
+        <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-2 py-2 rounded-l-lg" to='/add'>
             <img className='w-5 h-5' src={assets.add_icon}/>
-            <p className='hidden md:block text-xl'>追加</p>
+            <p className='hidden md:block'>追加</p>
         </NavLink>
-        <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-4 py-2 rounded-l-lg" to='/list'>
+        <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-2 py-2 rounded-l-lg" to='/list'>
             <img className='w-5 h-5' src={assets.order_icon}/>
-            <p className='hidden md:block text-xl'>注文リスト</p>
+            <p className='hidden md:block'>注文リスト</p>
         </NavLink>
-        <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-4 py-2 rounded-l-lg" to='/orders'>
+        <NavLink className="flex items-center gap-3 border border-gray-300 border-r-0 px-2 py-2 rounded-l-lg" to='/orders'>
             <img className='w-5 h-5' src={assets.order_icon}/>
-            <p className='hidden md:block text-xl'>注文</p>
+            <p className='hidden md:block'>注文</p>
         </NavLink>
       </div>
     </div>
