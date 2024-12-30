0f56d4f3b370e3cea86977631cbecae358489a64
navincograph179 <navin_gurung@co-graph.com>
2024-12-31 01:14:22 +0900

    admin panel login and add section

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SideBar from './components/Siderbar';
@@ -6,25 +6,43 @@ import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add';
 import List from './pages/List';
 import Orders from './pages/Orders';
+import Login from './components/Login';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+//import env(backend api) and export as backendURL to use.
+export const backendURL = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
+
+  const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'): "")
+
+  // to prevent from auto logout by using local storage
+  useEffect(()=> {
+    localStorage.setItem('token', token)
+  }, [token])
+
   return (
     <BrowserRouter>
-     <div className='bg-gray-50 min-h-screen'>
-    <>
-    <Navbar />
-    <hr />
-    <div className='flex w-full'>
-      <SideBar />
-      <div className='w-70% mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base'>
-        <Routes>
-          <Route path='/add' element = {<Add />} />
-          <Route path='/list' element = {<List />} />
-          <Route path='/orders' element = {<Orders />} />
-        </Routes>
-      </div>
-    </div>
-    </>
+      <div className='bg-gray-50 min-h-screen'>
+        <ToastContainer />
+    {token === '' ? <Login setToken= {setToken}/>:
+     <>
+     <Navbar  setToken = {setToken}/>
+     <hr />
+     <div className='flex w-full'>
+       <SideBar />
+       <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base'>
+         <Routes>
+          {/* why token, because of permission to add,list and orders */}
+           <Route path='/add' element = {<Add token = {token} />} />
+           <Route path='/list' element = {<List token = {token} />} />
+           <Route path='/orders' element = {<Orders token = {token} />} />
+         </Routes>
+       </div>
+     </div>
+     </>
+    }
     </div>
     </BrowserRouter>
    
