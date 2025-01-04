f4a373c85ded9ba9aa71c4f693f87fa66546ab95
navincograph179 <navin_gurung@co-graph.com>
2025-01-04 14:59:59 +0900

    useContext token state and signup login jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,32 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { ShopContext } from '../context/ShopContext'
+import axios from "axios";
 
 const Login = () => {
-  const [currentState, setCurrentState] = useState('ログイン画面');
+  const [currentState, setCurrentState] = useState('登録画面');
+
+  const { token, setToken, navigate, backendURL } = useContext(ShopContext)
+
+  // we want to store name, email and password of sign up display and link to the input field;
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   // prevent from reloading the page
   const onSubmitHandler = async(event) => {
     event.preventDefault();
+    try {
+      // is signup that this api will called otherwise login
+        if (currentState === '登録画面') {
+          const response = await axios.post(backendURL + 'api/users/register', {name, email, password})
+          console.log(response.data);
+          
+        }
+        else {
+
+        }
+    } catch (error) {
+      
+    }
   }
   return (
     <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800">
@@ -12,9 +34,9 @@ const Login = () => {
         <p className="text-3xl">{currentState}</p>
       </div>
 
-      {currentState === 'ログイン画面' ? '' : <input type="text" className="w-full px-3 py-2 border border-gray-800" placeholder="名前" required/>}
-      <input type="text" className="w-full px-3 py-2 border border-gray-800" placeholder="メールアドレス" required/>
-      <input type="password" className="w-full px-3 py-2 border border-gray-800" placeholder="パスワード" required/>
+      {currentState === 'ログイン画面' ? '' : <input onChange={(e)=> setName(e.target.value)} value={name} type="text" className="w-full px-3 py-2 border border-gray-800" placeholder="名前" required/>}
+      <input onChange={(e)=> setEmail(e.target.value)} value={email} type="text" className="w-full px-3 py-2 border border-gray-800" placeholder="メールアドレス" required/>
+      <input onChange={(e)=> setPassword(e.target.value)} value={password} type="password" className="w-full px-3 py-2 border border-gray-800" placeholder="パスワード" required/>
       <div className="w-full flex justify-between text-sm mt-[-8px]">
         <p className="cursor-pointer underline text-red-600">パスワードをお忘れの方</p>
         {
